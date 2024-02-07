b693fc5c50201305748f32e66b391daba3709096
juanfra312003 <80067016+juanfra312003@users.noreply.github.com>
2024-02-07 08:55:55 -0500

    Create factorial_solution.py

---
+++ factorial_solution.py
@@ -0,0 +1,12 @@
+import itertools
+
+def factorial_solution(lst, target):
+    retorno = set()
+    seen = set()
+    permutations = list(itertools.permutations(lst, 2))
+    for i in permutations:
+        if i[0] + i[1] == target:
+            if i not in seen and (i[1], i[0]) not in seen:
+                retorno.add(i)
+                seen.add(i)
+    return retorno

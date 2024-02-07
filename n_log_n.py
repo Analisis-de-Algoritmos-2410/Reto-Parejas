4843a3939a3f88f00ab3f998ac2632523c43ff5a
Esteban Salazar <68300629+Estebans441@users.noreply.github.com>
2024-02-07 07:45:59 -0500

    O(n log n) solution

---
+++ n_log_n.py
@@ -0,0 +1,21 @@
+def binary_search(inf, sup, list, tar):
+  if inf > sup:
+    return -1
+  else:
+    medio = (inf + sup) // 2
+    if list[medio] == tar:
+      return list[medio]
+    elif list[medio] > tar:
+      return binary_search(inf, medio - 1, list, tar)
+    else:
+      return binary_search(medio + 1, sup, list, tar)
+
+def n_log_n(arr, tar):
+  ans = []
+  arr.sort()
+  for i in range(len(arr)//2):
+    aj = binary_search(0, len(arr)-1, list, tar- arr[i])
+    if aj != -1:
+      ans.append((arr[i], aj))
+  return ans
+  
